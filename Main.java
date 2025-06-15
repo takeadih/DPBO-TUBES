6959d01e85f11a81c0832da41c42fcf26bc3c7b8
takeadih <razkarabbanynurdiyana@gmail.com>
2025-06-15 22:57:32 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package TUBES.dpbotubes;
+
+import Class.GadgetStore;
+
+public class Main {
+    public static void main(String[] args) {
+        GadgetStore store = new GadgetStore();
+        store.start();
+    }
+}
+
